52251184192e6467c04eb57bb172d7bcc38bce0c
HirokiKuroda <kinaco.ao@gmail.com>
2024-01-31 00:31:22 +0900

    取得した記事一覧情報を表示するように修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import { client } from '@/libs/client';
 import { ListItem } from '@/components/ListItem';
-import { Articles } from '@/app/types/article';
+import { Articles } from '@/types/article';
 
 const Home: () => Promise<JSX.Element> = async () => {
-  const data: Articles = await client.get({
+  const articleData: Articles = await client.get({
     endpoint: 'articles',
     queries: { offset: 0 },
   });
-
-  console.log(data);
 
   return (
     <main className="h-[calc(100vh_-_68px)]">
@@ -16,9 +14,9 @@ const Home: () => Promise<JSX.Element> = async () => {
         記事一覧
       </h1>
       <div className="container mx-auto p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-        {Array.from({ length: 6 }).map((_, i) => {
-          return <ListItem key={`list-${i}`} />;
-        })}
+        {articleData.contents.map((content, i) => (
+          <ListItem key={`list-${i}`} content={content} />
+        ))}
       </div>
     </main>
   );
--- src/components/listItem.tsx
+++ src/components/listItem.tsx
@@ -1,19 +1,30 @@
-import Image from 'next/image';
+'use client';
+// 一旦use clientを使用
+//Error: Functions cannot be passed directly to Client Components unless you explicitly expose it by marking it with "use server".
 
-export const ListItem: React.FC = () => {
+import Image from 'next/image';
+import { Content } from '@/types/article';
+
+const microCMSLoader = ({ src, width }: { src: string; width: number }) =>
+  `${src}?auto=format&fit=max&w=${width}`;
+
+export const ListItem: React.FC<{ content: Content }> = (props) => {
+  const { content } = props;
+
   return (
     <div className="rounded overflow-hidden shadow-lg">
       <Image
+        loader={microCMSLoader}
         className="w-full"
-        src="/testImage.jpg"
+        src={content.eyecatch.url}
         alt="Sunset in the mountains"
         width={300}
         height={200}
       />
-      <div className="px-6 py-4">タイトル</div>
+      <div className="px-6 py-4">{content.title}</div>
       <div className="px-6 pt-4 pb-2">
         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          # タグ
+          #{content.category.name}
         </span>
       </div>
     </div>
